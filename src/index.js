5d9c2710fe7bc9b4f49361923e6256e7f67ef056
Ere-yiga <erehdagr8@gmail.com>
2025-06-06 19:54:42 +0100

    final touches

--- src/index.js
+++ src/index.js
@@ -3,15 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function App(){
-
-}
-function Card(){
   return(
-    <div>
-      <img src="anime.jpg" alt="a dp"/>
+    <div className='card'>
+      <Image />
+      <div className='data'>
+        <Intro />
+      </div>
     </div>
   )
 }
+
+function Image(){
+  return(
+    <img src="anime.jpg" className ="image" alt="Ereyiga" />
+  )
+}
+function Intro(){
+  return(
+    <div className='data'>
+      <h1>Ereyiga</h1>
+      <p>
+        Hey there! I am Ereyiga, an aspiring full stack dev buh currently in the frontend phase, learning React.
+        Beyond coding, I enjoy playing socccer, be it gaming online with friends or hitting the field for a match. 
+      </p>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
